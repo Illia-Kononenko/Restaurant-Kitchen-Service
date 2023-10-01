f3636f2e950f1e9fbdff6e78c4b08a6766bd393c
Illia <kononenko2109@gmail.com>
2023-10-01 19:38:38 +0300

    added styles.css

--- restaurant_kitchen_service/asgi.py
+++ restaurant_kitchen_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for restaurant_kitchen_service project.
+ASGI restaurant_kitchen_service for restaurant_kitchen_service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,8 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "restaurant_kitchen_service.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurant_kitchen_service.settings")
 
 application = get_asgi_application()
